5392d047cd6692043303627103ea8cba7f3ccad5
museun <museun@outlook.com>
2019-03-12 02:02:56 -0400

    organize a bit

--- src/bin/streamchatd.rs
+++ src/bin/streamchatd.rs
@@ -6,7 +6,7 @@ use hashbrown::HashMap;
 use log::*;
 use serde::{Deserialize, Serialize};
 
-use twitchchat::twitch::Client;
+use twitchchat::twitch::{commands::PrivMsg, Client, Message as TwitchMsg, RGB};
 
 use streamchat::{
     transports,   //
@@ -19,7 +19,81 @@ use streamchat::{
     Version,      //
 };
 
-// TODO oauth implicit flow grant
+trait RelativeColor {
+    fn is_dark(self) -> bool;
+    fn is_light(self) -> bool;
+}
+
+impl RelativeColor for RGB {
+    fn is_dark(self) -> bool {
+        let HSL(.., l) = self.into();
+        l < 30.0 // random number
+    }
+
+    fn is_light(self) -> bool {
+        let HSL(.., l) = self.into();
+        l < 80.0 // random number
+    }
+}
+
+#[derive(PartialEq, Copy, Clone, Debug)]
+pub struct HSL(pub f64, pub f64, pub f64); // H S L
+
+impl std::fmt::Display for HSL {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let HSL(h, s, l) = self;
+        write!(f, "{:.2}%, {:.2}%, {:.2}%", h, s, l)
+    }
+}
+
+impl From<RGB> for HSL {
+    fn from(RGB(r, g, b): RGB) -> Self {
+        #![allow(clippy::unknown_clippy_lints, clippy::many_single_char_names)]
+        use std::cmp::{max, min};
+
+        let max = max(max(r, g), b);
+        let min = min(min(r, g), b);
+        let (r, g, b) = (
+            f64::from(r) / 255.0,
+            f64::from(g) / 255.0,
+            f64::from(b) / 255.0,
+        );
+
+        let (min, max) = (f64::from(min) / 255.0, f64::from(max) / 255.0);
+        let l = (max + min) / 2.0;
+        let delta: f64 = max - min;
+        // this checks for grey
+        if delta == 0.0 {
+            return HSL(0.0, 0.0, ((l * 100.0).round() / 100.0) * 100.0);
+        }
+
+        let s = if l < 0.5 {
+            delta / (max + min)
+        } else {
+            delta / (2.0 - max - min)
+        };
+
+        let r2 = (((max - r) / 6.0) + (delta / 2.0)) / delta;
+        let g2 = (((max - g) / 6.0) + (delta / 2.0)) / delta;
+        let b2 = (((max - b) / 6.0) + (delta / 2.0)) / delta;
+
+        let h = match match max {
+            x if (x - r).abs() < 0.001 => b2 - g2,
+            x if (x - g).abs() < 0.001 => (1.0 / 3.0) + r2 - b2,
+            _ => (2.0 / 3.0) + g2 - r2,
+        } {
+            h if h < 0.0 => h + 1.0,
+            h if h > 1.0 => h - 1.0,
+            h => h,
+        };
+
+        let h = (h * 360.0 * 100.0).round() / 100.0;
+        let s = ((s * 100.0).round() / 100.0) * 100.0;
+        let l = ((l * 100.0).round() / 100.0) * 100.0;
+
+        HSL(h, s, l)
+    }
+}
 
 #[derive(Debug, Deserialize, Serialize)]
 struct Config {
@@ -48,117 +122,6 @@ impl Configurable for Config {
         "streamchatd.toml"
     }
 }
-
-fn main() {
-    let config = match Config::load() {
-        Ok(config) => config,
-        Err(Error::Read(..)) => {
-            let dir = Config::dir(); // this is probably not the right thing to do
-            eprintln!("creating default config.");
-            eprintln!("look for it at {}", dir.to_string_lossy());
-            Config::default().save().expect("save new config");
-            std::process::exit(2)
-        }
-        Err(err) => {
-            eprintln!("cannot load config: {}", err);
-            std::process::exit(1)
-        }
-    };
-
-    let args = env::args().skip(1).collect::<Vec<_>>();
-    let args = Args::parse(&args).unwrap_or_default();
-
-    let limit = args.get_as("-l", config.limit, |s| s.parse::<usize>().ok());
-    let channel = args.get("-c", &config.channel);
-    let nick = args.get("-n", &config.nick);
-
-    match (
-        channel.is_empty(),
-        nick.is_empty(),
-        config.oauth_token.is_empty(),
-    ) {
-        (true, _, _) => {
-            eprintln!("`channel` is invalid, or use the arg: -c <channel>");
-            std::process::exit(1)
-        }
-        (_, true, _) => {
-            eprintln!("`nick` is invalid. or use the arg: -n <nick>");
-            std::process::exit(1)
-        }
-        (_, _, true) => {
-            eprintln!("`oauth_token` is invalid. modify the config",);
-            std::process::exit(1)
-        }
-        _ => {}
-    }
-
-    let color = env::var("NO_COLOR").is_err();
-    env_logger::Builder::from_default_env()
-        .default_format_timestamp(false)
-        .write_style(if !color {
-            env_logger::WriteStyle::Never
-        } else {
-            env_logger::WriteStyle::Auto
-        })
-        .init();
-
-    use twitchchat::{twitch::Client, UserConfig};
-
-    info!("connecting to: {}", twitchchat::TWITCH_IRC_ADDRESS);
-    let (read, write) = {
-        let read = TcpStream::connect(twitchchat::TWITCH_IRC_ADDRESS).unwrap();
-        let write = read.try_clone().unwrap();
-        (read, write)
-    };
-    info!("opened connection");
-
-    let mut client = Client::new(read, write);
-    let conf = UserConfig::builder()
-        .nick(nick)
-        .token(&config.oauth_token)
-        .build()
-        .expect("valid configuration");
-
-    info!("registering with nick: {}", conf.nick);
-    client.register(conf).unwrap();
-
-    let user = match client.wait_for_ready() {
-        Ok(user) => user,
-        Err(twitchchat::twitch::Error::InvalidRegistration) => {
-            error!("invalid nick/pass. check the configuration");
-            std::process::exit(1);
-        }
-        Err(err) => {
-            error!("cannot complete the connection: {}", err);
-            std::process::exit(1);
-        }
-    };
-
-    info!(
-        "connected with {} ({}).",
-        user.display_name.unwrap(),
-        user.user_id
-    );
-
-    let channel = format!("#{}", config.channel);
-    client.join(&channel).unwrap();
-    info!("joined: {}", &channel);
-
-    let mut processor = CommandProcessor::default();
-    processor.add("color", handle_color);
-
-    let transports: Vec<Box<dyn Transport>> = vec![
-        Box::new(transports::Socket::start(&config.address, limit)), // socket transport
-    ];
-
-    if let Err(err) = Service::new(client, transports, processor).run() {
-        error!("error running service: {}", err);
-        std::process::exit(1)
-    }
-}
-
-use twitchchat::twitch::commands::PrivMsg;
-use twitchchat::twitch::Message as TwitchMsg;
 
 struct Service<R, W> {
     client: Client<R, W>,
@@ -323,80 +286,112 @@ impl CommandProcessor {
     }
 }
 
-use twitchchat::twitch::RGB;
+// TODO oauth implicit flow grant
 
-trait RelativeColor {
-    fn is_dark(self) -> bool;
-    fn is_light(self) -> bool;
-}
-
-impl RelativeColor for RGB {
-    fn is_dark(self) -> bool {
-        let HSL(.., l) = self.into();
-        l < 30.0 // random number
-    }
-
-    fn is_light(self) -> bool {
-        let HSL(.., l) = self.into();
-        l < 80.0 // random number
-    }
-}
-
-#[derive(PartialEq, Copy, Clone, Debug)]
-pub struct HSL(pub f64, pub f64, pub f64); // H S L
-
-impl std::fmt::Display for HSL {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        let HSL(h, s, l) = self;
-        write!(f, "{:.2}%, {:.2}%, {:.2}%", h, s, l)
-    }
-}
-
-impl From<RGB> for HSL {
-    fn from(RGB(r, g, b): RGB) -> Self {
-        #![allow(clippy::unknown_clippy_lints, clippy::many_single_char_names)]
-        use std::cmp::{max, min};
-
-        let max = max(max(r, g), b);
-        let min = min(min(r, g), b);
-        let (r, g, b) = (
-            f64::from(r) / 255.0,
-            f64::from(g) / 255.0,
-            f64::from(b) / 255.0,
-        );
-
-        let (min, max) = (f64::from(min) / 255.0, f64::from(max) / 255.0);
-        let l = (max + min) / 2.0;
-        let delta: f64 = max - min;
-        // this checks for grey
-        if delta == 0.0 {
-            return HSL(0.0, 0.0, ((l * 100.0).round() / 100.0) * 100.0);
+fn main() {
+    let config = match Config::load() {
+        Ok(config) => config,
+        Err(Error::Read(..)) => {
+            let dir = Config::dir(); // this is probably not the right thing to do
+            eprintln!("creating default config.");
+            eprintln!("look for it at {}", dir.to_string_lossy());
+            Config::default().save().expect("save new config");
+            std::process::exit(2)
         }
+        Err(err) => {
+            eprintln!("cannot load config: {}", err);
+            std::process::exit(1)
+        }
+    };
 
-        let s = if l < 0.5 {
-            delta / (max + min)
+    let args = env::args().skip(1).collect::<Vec<_>>();
+    let args = Args::parse(&args).unwrap_or_default();
+
+    let limit = args.get_as("-l", config.limit, |s| s.parse::<usize>().ok());
+    let channel = args.get("-c", &config.channel);
+    let nick = args.get("-n", &config.nick);
+
+    match (
+        channel.is_empty(),
+        nick.is_empty(),
+        config.oauth_token.is_empty(),
+    ) {
+        (true, _, _) => {
+            eprintln!("`channel` is invalid, or use the arg: -c <channel>");
+            std::process::exit(1)
+        }
+        (_, true, _) => {
+            eprintln!("`nick` is invalid. or use the arg: -n <nick>");
+            std::process::exit(1)
+        }
+        (_, _, true) => {
+            eprintln!("`oauth_token` is invalid. modify the config",);
+            std::process::exit(1)
+        }
+        _ => {}
+    }
+
+    let color = env::var("NO_COLOR").is_err();
+    env_logger::Builder::from_default_env()
+        .default_format_timestamp(false)
+        .write_style(if !color {
+            env_logger::WriteStyle::Never
         } else {
-            delta / (2.0 - max - min)
-        };
+            env_logger::WriteStyle::Auto
+        })
+        .init();
 
-        let r2 = (((max - r) / 6.0) + (delta / 2.0)) / delta;
-        let g2 = (((max - g) / 6.0) + (delta / 2.0)) / delta;
-        let b2 = (((max - b) / 6.0) + (delta / 2.0)) / delta;
+    use twitchchat::{twitch::Client, UserConfig};
 
-        let h = match match max {
-            x if (x - r).abs() < 0.001 => b2 - g2,
-            x if (x - g).abs() < 0.001 => (1.0 / 3.0) + r2 - b2,
-            _ => (2.0 / 3.0) + g2 - r2,
-        } {
-            h if h < 0.0 => h + 1.0,
-            h if h > 1.0 => h - 1.0,
-            h => h,
-        };
+    info!("connecting to: {}", twitchchat::TWITCH_IRC_ADDRESS);
+    let (read, write) = {
+        let read = TcpStream::connect(twitchchat::TWITCH_IRC_ADDRESS).unwrap();
+        let write = read.try_clone().unwrap();
+        (read, write)
+    };
+    info!("opened connection");
 
-        let h = (h * 360.0 * 100.0).round() / 100.0;
-        let s = ((s * 100.0).round() / 100.0) * 100.0;
-        let l = ((l * 100.0).round() / 100.0) * 100.0;
+    let mut client = Client::new(read, write);
+    let conf = UserConfig::builder()
+        .nick(nick)
+        .token(&config.oauth_token)
+        .build()
+        .expect("valid configuration");
 
-        HSL(h, s, l)
+    info!("registering with nick: {}", conf.nick);
+    client.register(conf).unwrap();
+
+    let user = match client.wait_for_ready() {
+        Ok(user) => user,
+        Err(twitchchat::twitch::Error::InvalidRegistration) => {
+            error!("invalid nick/pass. check the configuration");
+            std::process::exit(1);
+        }
+        Err(err) => {
+            error!("cannot complete the connection: {}", err);
+            std::process::exit(1);
+        }
+    };
+
+    info!(
+        "connected with {} ({}).",
+        user.display_name.unwrap(),
+        user.user_id
+    );
+
+    let channel = format!("#{}", config.channel);
+    client.join(&channel).unwrap();
+    info!("joined: {}", &channel);
+
+    let mut processor = CommandProcessor::default();
+    processor.add("color", handle_color);
+
+    let transports: Vec<Box<dyn Transport>> = vec![
+        Box::new(transports::Socket::start(&config.address, limit)), // socket transport
+    ];
+
+    if let Err(err) = Service::new(client, transports, processor).run() {
+        error!("error running service: {}", err);
+        std::process::exit(1)
     }
 }
