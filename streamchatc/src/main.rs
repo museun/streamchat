b8d59ec3ffea0a444d2ef3f4f0b21f9e0b481830
museun <museun@outlook.com>
2019-07-21 21:06:31 -0400

    remove debug address

--- streamchatc/src/main.rs
+++ streamchatc/src/main.rs
@@ -13,14 +13,12 @@ use args::Args;
 use yansi::{Color, Paint};
 
 fn main() {
-    let mut config = Args::load_or_config();
+    let config = Args::load_or_config();
     let color = std::env::var("NO_COLOR").is_err();
 
     if cfg!(windows) && !Paint::enable_windows_ascii() || !color {
         Paint::disable();
     }
-
-    config.address = "localhost:51002".into();
 
     Window::run(config);
 }
